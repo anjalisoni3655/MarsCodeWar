943d6cf58b615e46c48c18738d14936b9440ed92
Bineeta Oram <54524155+Bineeta2158@users.noreply.github.com>
2020-07-24 19:43:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ var copy = function (obj) {
 	var newObject = jQuery.extend(true, {}, obj);
 	return newObject;
   };
+	function sound(src) {
+	  this.sound = document.createElement("audio");
+	  this.sound.src = src;
+	  this.sound.setAttribute("preload", "auto");
+	  this.sound.setAttribute("controls", "none");
+	  this.sound.style.display = "none";
+	  document.body.appendChild(this.sound);
+	  this.play = function(){
+	    this.sound.play();
+	  }
+	  this.stop = function(){
+	    this.sound.pause();
+	  }
+	}
   var Cursor = function (jQueryString) {
 	// private fields
 	var element = jQuery(jQueryString);
@@ -787,6 +801,10 @@ var copy = function (obj) {
 	  }
 	  return ["XO"];
 	};
+	  var mySound;
+	mySound= new sound("sound.mp3");
+	var error;
+	error=new sound("error.mp3");
 	var winCheckAction = function (check) {
 	  switch (check[0]) {
 		case "X":
@@ -798,7 +816,7 @@ var copy = function (obj) {
 		  window.setTimeout(function () {
 			document.getElementById("won-popup").style.display = "none";
 		  }, 2000);
-
+                  mySound.play();
 		  break;
 		case "O":
 		  score.O++;
@@ -809,6 +827,7 @@ var copy = function (obj) {
 		  window.setTimeout(function () {
 			document.getElementById("won-popup2").style.display = "none";
 		  }, 2000);
+                  mySound.play();
 		  break;
 		case "XO":
 		  window.setTimeout(function () {
@@ -817,6 +836,7 @@ var copy = function (obj) {
 		  window.setTimeout(function () {
 			document.getElementById("won-popup3").style.display = "none";
 		  }, 2000);
+			  error.play();
 		case "OX":
 		  window.setTimeout(function () {
 			document.getElementById("won-popup3").style.display = "block";
@@ -824,6 +844,7 @@ var copy = function (obj) {
 		  window.setTimeout(function () {
 			document.getElementById("won-popup3").style.display = "none";
 		  }, 2000);
+			  error.play();
 		  break;
 		default:
 		  changePlayerTurn();
